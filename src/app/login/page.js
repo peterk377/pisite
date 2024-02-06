dec4a6c3d8ba43198c6cc340f14284f74450a464
peterk377 <peterk377@gmail.com>
2024-02-06 21:09:10 +0000

    small changes

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,1 +1,17 @@
-<p>word</p>
+export default function Login() {
+    return (
+      <main>
+        <div class = 'nav'>
+          <h1>PiAlert</h1>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Alerts</a></li>
+            <li><a href="">Recordings</a></li>
+            <li><a href="">Account</a></li>
+          </ul>
+        </div>
+  
+      </main>
+    );
+  }
+  
