18a8e916e33de80d118511fc9f4f1a65ea29f942
peterk377 <peterk377@gmail.com>
2024-02-12 18:59:11 +0000

    All alerts show up on the alerts page

--- src/app/alerts/page.js
+++ src/app/alerts/page.js
@@ -1,20 +1,40 @@
-export default function Alerts() {
-    return (
-      <main>
-        <div class = 'nav'>
-          <h1>Alerts</h1>
-          <ul>
-            <li><a href="../">Home</a></li>
-            <li><a href="../alerts">Alerts</a></li>
-            <li><a href="../recordings">Recordings</a></li>
-            <li><a href="../contact">Contact</a></li>
-            <li><a href="../account">Account</a></li>
-          </ul>
-        </div>
+'use client';
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import {useEffect, useState} from "react";
 
-        
-  
-      </main>
-    );
-  }
-  
+export default function Page() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('api/getAlerts')
+    .then((res) => res.json())
+    .then((data) => {setData(data)})
+  }, [])
+
+  if (!data) return <p>Loading</p>
+
+  return (
+    <div>
+    <div>Alerts</div>
+    <div>
+      {data.map((alert, a) => (
+        <div key = {a}>
+          AlertID: {alert.alertID}
+          <br></br>
+          {alert.date}
+          <br></br>
+          {alert.video}
+          <br></br>
+          {alert.image}
+          <br></br>
+          <br></br>
+
+        </div>
+      ))
+      }
+    </div>
+    </div>
+);
+}
