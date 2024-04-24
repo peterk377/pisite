83a3de55fd5ea9829de5e975044c1a3e1ce0cacd
Mon <98190812+MonPanitan@users.noreply.github.com>
2024-04-24 19:27:57 +0100

    separate page for alerts and download
    User can see the separate page for each alert depending on the ID and user can download the file to their local machine.

---
+++ src/template/loading.js
@@ -0,0 +1,14 @@
+import "../style/loading.css";
+
+const loading = () => {
+  return (
+    <div class="typing-indicator">
+      <div class="typing-circle"></div>
+      <div class="typing-circle"></div>
+      <div class="typing-circle"></div>
+      <div class="typing-shadow"></div>
+      <div class="typing-shadow"></div>
+      <div class="typing-shadow"></div>
+    </div>
+  );
+};
