592a6aa2af23fb613732c3f2d315144566aed656
peterk377 <peterk377@gmail.com>
2024-04-29 15:46:25 +0100

    added nav to contact page

--- components/nav.js
+++ components/nav.js
@@ -7,14 +7,14 @@ function ResponsiveAppBar() {
     <div className="navbar">
       <nav>
         <div className="wrapper">
-          <div className="logo"><a href="#">PiAlert</a></div>
+          <div className="logo"><a href="./">PiAlert</a></div>
           <input type="radio" name="slider" id="menu-btn" />
           <input type="radio" name="slider" id="close-btn" />
           <ul className="nav-links">
             <label htmlFor="close-btn" className="btn close-btn"><i className="fas fa-times"></i></label>
             <li><a href="./userLanding">Dashboard</a></li>
             <li><a href="./alerts">Alerts</a></li>
-            <li><a href="./conract">Contact</a></li>
+            <li><a href="./contact">Contact</a></li>
             <li>
         <a href="#" class="desktop-item">Account</a>
         <input type="checkbox" id="showDrop"></input>
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,20 +1,11 @@
-export default function Contact() {
-    return (
-      <main>
-        <div class = 'nav'>
-          <h1>Contact</h1>
-          <ul>
-            <li><a href="../">Home</a></li>
-            <li><a href="../alerts">Alerts</a></li>
-            <li><a href="../recordings">Recordings</a></li>
-            <li><a href="../contact">Contact</a></li>
-            <li><a href="../account">Account</a></li>
-          </ul>
-        </div>
+import ResponsiveAppBar from "../../../components/nav";
+import Contact from "../../../components/contact";
 
-        <h1>Hi! feel free to contact us by pressing <a href="mailto:pialerttest@gmail.com">here</a></h1>
-        <h1>Or just shoot us a message at pialerttest@gmail.com</h1>
-  
-      </main>
+export default function ContactPage() {
+    return (
+      <div>
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <Contact></Contact>
+      </div>
     );
   }
