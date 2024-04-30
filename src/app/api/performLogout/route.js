950837b7bc4deff2d8b877e01d4531497f1fd064
Mon <98190812+MonPanitan@users.noreply.github.com>
2024-04-30 16:17:17 +0100

    Update performLogout
    Update a return json

--- src/app/api/performLogout/route.js
+++ src/app/api/performLogout/route.js
@@ -3,4 +3,5 @@ import { cookies } from "next/headers";
 export async function GET(req, res) {
   // Make a note we are on
   // the api. This goes to the console.
+  return Response.json({ status: "logged out" });
 }
